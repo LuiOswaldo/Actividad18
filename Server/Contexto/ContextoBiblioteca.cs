328cf2eb747f6f775c452b5d398c63e8cdbd0804
pioli <pioli@LAPTOP-HB9D36Q7>
2023-05-22 00:41:16 -0600

    Agregar archivos de proyecto.

---
+++ Server/Contexto/ContextoBiblioteca.cs
@@ -0,0 +1,16 @@
+﻿using Actividad18.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad18.Server.Contexto
+{
+    public class ContextoBiblioteca : DbContext
+    {
+        public ContextoBiblioteca(DbContextOptions<ContextoBiblioteca> options) : base (options) 
+        {
+            
+        }
+        public DbSet<Libro> Libro { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+    }
+}
---
+++ Shared/Models/Libros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad17.Shared.Models
+{
+    public class Libros
+    {
+        public int? Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Fecha { get; set; }
+        public int? veces { get; set; }
+    }
+}
